2844b52d5170942d1adeffae6815397b0bf4fccb
Silvina Delgado <silvina.ruiz@gmail.com>
2018-10-15 10:54:25 -0500

    Survey

--- src/components/survey/survey.js
+++ src/components/survey/survey.js
@@ -5,39 +5,137 @@ const Survey = () => {
    <div>
      <ol>
         <li>
-          <h3>I try to see myself in other peoples shoes.</h3>
-              <div clasName='radio'>
-                 <input type="radio" className="radioCustomButton" name="radioGroup"/> Strongly Agree
-                 <input type="radio" className="radioCustomButton" name="radioGroup"/> Agree
-                 <input type="radio" className="radioCustomButton" name="radioGroup"/> Neutral
-                 <input type="radio" className="radioCustomButton" name="radioGroup"/> Disagree
-                 <input type="radio" className="radioCustomButton" name="radioGroup"/> Strongly Disagree
-               </div >
+          <h3>1. I try to see myself in other people's shoes.</h3>
+          <form>
+            <div class="radios">
+               <input className="ph1" class="radio-input" type="radio" name="test"/>
+               <label class="radio-label">Strongly Agree</label>
+               <input className="ph1"  class="radio-input" type="radio" name="test"/>
+               <label class="radio-label">Agree</label>
+               <input className="ph1"  class="radio-input" type="radio" name="test"/>
+               <label className="ph1" class="radio-label">Neutral</label>
+               <input class="radio-input" type="radio" name="test"/>
+               <label className="ph1" class="radio-label">Disagree</label>
+               <input className="ph1" class="radio-input" type="radio" name="test"/>
+               <label class="radio-label">Strongly Disagree</label>
+            </div>
+          </form>
         </li>
 
         <li>
-          <h3>I try to see myself in other peoples shoes.</h3>
-             <div clasName='radio'>
-              <input type="radio" className="radioCustomButton" name="radioGroup"/> Strongly Agree
-              <input type="radio" className="radioCustomButton" name="radioGroup"/> Agree
-              <input type="radio" className="radioCustomButton" name="radioGroup"/> Neutral
-              <input type="radio" className="radioCustomButton" name="radioGroup"/> Disagree
-              <input type="radio" className="radioCustomButton" name="radioGroup"/> Strongly Disagree
-              </div >
+         <h3>2. I feel sorry for people in a worse situation than myself.</h3>
+         <form>
+           <div class="radios">
+              <input className="ph1" class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Strongly Agree</label>
+              <input className="ph1"  class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Agree</label>
+              <input className="ph1"  class="radio-input" type="radio" name="test"/>
+              <label className="ph1" class="radio-label">Neutral</label>
+              <input class="radio-input" type="radio" name="test"/>
+              <label className="ph1" class="radio-label">Disagree</label>
+              <input className="ph1" class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Strongly Disagree</label>
+           </div>
+         </form>
+       </li>
+
+        <li>
+          <h3>3. Friends often turn to me for advice.</h3>
+          <form>
+            <div class="radios">
+               <input className="ph1" class="radio-input" type="radio" name="test"/>
+               <label class="radio-label">Strongly Agree</label>
+               <input className="ph1"  class="radio-input" type="radio" name="test"/>
+               <label class="radio-label">Agree</label>
+               <input className="ph1"  class="radio-input" type="radio" name="test"/>
+               <label className="ph1" class="radio-label">Neutral</label>
+               <input class="radio-input" type="radio" name="test"/>
+               <label className="ph1" class="radio-label">Disagree</label>
+               <input className="ph1" class="radio-input" type="radio" name="test"/>
+               <label class="radio-label">Strongly Disagree</label>
+            </div>
+          </form>
         </li>
 
         <li>
-          <h3>Friends often turn to me for advice.</h3>
-             <div clasName='radio'>
-              <input type="radio" className="radioCustomButton" name="radioGroup"/> Strongly Agree
-              <input type="radio" className="radioCustomButton" name="radioGroup"/> Agree
-              <input type="radio" className="radioCustomButton" name="radioGroup"/> Neutral
-              <input type="radio" className="radioCustomButton" name="radioGroup"/> Disagree
-              <input type="radio" className="radioCustomButton" name="radioGroup"/> Strongly Disagree
-              </div >
+          <h3>4. I sometimes find that I am feeling sorry for myself.</h3>
+          <form>
+            <div class="radios">
+               <input className="ph1" class="radio-input" type="radio" name="test"/>
+               <label class="radio-label">Strongly Agree</label>
+               <input className="ph1"  class="radio-input" type="radio" name="test"/>
+               <label class="radio-label">Agree</label>
+               <input className="ph1"  class="radio-input" type="radio" name="test"/>
+               <label className="ph1" class="radio-label">Neutral</label>
+               <input class="radio-input" type="radio" name="test"/>
+               <label className="ph1" class="radio-label">Disagree</label>
+               <input className="ph1" class="radio-input" type="radio" name="test"/>
+               <label class="radio-label">Strongly Disagree</label>
+            </div>
+          </form>
         </li>
 
-     </ol>
+        <li>
+         <h3>5. I enjoy imaginative stories.</h3>
+         <form>
+           <div class="radios">
+              <input className="ph1" class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Strongly Agree</label>
+              <input className="ph1"  class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Agree</label>
+              <input className="ph1"  class="radio-input" type="radio" name="test"/>
+              <label className="ph1" class="radio-label">Neutral</label>
+              <input class="radio-input" type="radio" name="test"/>
+              <label className="ph1" class="radio-label">Disagree</label>
+              <input className="ph1" class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Strongly Disagree</label>
+           </div>
+         </form>
+       </li>
+
+        <li>
+          <h3>6. I often experience unbelievable dreams.</h3>
+          <form>
+            <div class="radios">
+              <input className="ph1" class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Strongly Agree</label>
+              <input className="ph1"  class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Agree</label>
+              <input className="ph1"  class="radio-input" type="radio" name="test"/>
+              <label className="ph1" class="radio-label">Neutral</label>
+              <input class="radio-input" type="radio" name="test"/>
+              <label className="ph1" class="radio-label">Disagree</label>
+              <input className="ph1" class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Strongly Disagree</label>
+            </div>
+          </form>
+       </li>
+
+        <li>
+          <h3>7. I like to stand out in a crowd.</h3>
+          <form>
+            <div class="radios">
+              <input className="ph1" class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Strongly Agree</label>
+              <input className="ph1"  class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Agree</label>
+              <input className="ph1"  class="radio-input" type="radio" name="test"/>
+              <label className="ph1" class="radio-label">Neutral</label>
+              <input class="radio-input" type="radio" name="test"/>
+              <label className="ph1" class="radio-label">Disagree</label>
+              <input className="ph1" class="radio-input" type="radio" name="test"/>
+              <label class="radio-label">Strongly Disagree</label>
+            </div>
+          </form>
+       </li>
+      </ol>
+          <div>
+          <button type="submit" class="btn">Submit Answers</button>
+          </div>
+          <div>
+          <button class="w3-button w3-light-grey w3-hover-black w3-round w3-display-middle">Button</button>
+          </div>
    </div>
  );
 }
