b512e60221db7f57c363d1e97e0c3f058e1a5a7c
ErikMarroquin <erikmarroquingo91@gmail.com>
2018-10-16 11:33:04 -0500

    get post

--- database/database.js
+++ database/database.js
@@ -5,3 +5,29 @@ var mysql = require('mysql');
   password : 'bayboss123',
   database : 'chat'
 });
+
+connection.connect(function(err) {
+ if (!err) {
+   console.log("Database is is connected...nn");
+ } else {
+   console.log("Error connecting database....nn");
+ }
+});
+
+
+
+
+const chatRoom = function(amount_mesuare, carbs, carbs_time, callback) {
+ connection.query(
+   "INSERT INTO carbs (amount_mesuare, carbs, carbs_time) VALUES (?, ?, ?)",
+   [amount_mesuare, carbs, carbs_time],
+   (err, results) => {
+     if (err) {
+       callback(err, null);
+     } else {
+       console.log(results);
+       callback(null, results);
+     }
+   }
+ );
+};
